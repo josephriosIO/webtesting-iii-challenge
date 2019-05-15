0d5bd1815ea7c3ea0167221924a91603c9f32597
josephriosio <blankworldwide@gmail.com>
2019-05-15 14:15:15 -0700

    added prop testing

--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -15,4 +15,26 @@ describe("Display.js", () => {
       expect(tree.toJSON()).toMatchSnapshot();
     });
   });
+  describe("prop test", () => {
+    it('show "Closed" when prop is true', () => {
+      const closed = true;
+      const { getByText } = render(<Display closed={closed} />);
+      getByText("Closed");
+    });
+    it('show "Opened" when prop is false', () => {
+      const closed = false;
+      const { getByText } = render(<Display closed={closed} />);
+      getByText("Open");
+    });
+    it('show "Locked" when  locked prop is true', () => {
+      const locked = true;
+      const { getByText } = render(<Display locked={locked} />);
+      getByText("Locked");
+    });
+    it('show "Unlocked" when  locked prop is false', () => {
+      const locked = false;
+      const { getByText } = render(<Display locked={locked} />);
+      getByText("Unlocked");
+    });
+  });
 });
