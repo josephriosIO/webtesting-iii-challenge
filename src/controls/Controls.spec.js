03e6d583ac8fb3fe8cc0a57dbb9baa7774d531bd
josephriosio <blankworldwide@gmail.com>
2019-05-15 14:04:57 -0700

    added test for controls

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,6 +1,40 @@
 // Test away!
-const controls = require("./Controls.js");
+import React from "react";
+import { render, fireEvent } from "react-testing-library";
+import "react-testing-library/cleanup-after-each";
+
+import Dashboard from "../dashboard/Dashboard";
+import Controls from "./Controls";
 
 describe("Controls.js", () => {
-  it("buttons to click to toggle closed and locked", () => {});
+  describe("check for toggle buttons", () => {
+    it("should display 2 buttons, Close Gate and Lock Gate", () => {
+      const { getByText } = render(<Controls />);
+      getByText("Close Gate");
+      getByText("Lock Gate");
+    });
+  });
+  describe("check for button text changes", () => {
+    it("should display Open Gate on Close Gate click", () => {
+      const { getByText } = render(<Dashboard />);
+      const closeGateButton = getByText("Close Gate");
+      fireEvent.click(closeGateButton);
+      expect(closeGateButton.textContent).toBe("Open Gate");
+    });
+    it("should display Unlock Gate on Lock Gate click", () => {
+      const { getByText } = render(<Dashboard />);
+      const lockGateButton = getByText("Lock Gate");
+      const closeGateButton = getByText("Close Gate");
+      fireEvent.click(closeGateButton);
+      fireEvent.click(lockGateButton);
+      expect(lockGateButton.textContent).toBe("Unlock Gate");
+    });
+  });
+  describe("Check if toggle buttons show disabled", () => {
+    it("closed toggle button should be disabed if gate is closed", () => {
+      const { getByText } = render(<Dashboard />);
+      const closeGateButton = getByText("Close Gate");
+      expect(closeGateButton.textContent).toBe("Close Gate");
+    });
+  });
 });
