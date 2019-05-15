fdeada7e0dd61bc605f684c98fe4ec1a27090d80
josephriosio <blankworldwide@gmail.com>
2019-05-15 14:07:15 -0700

    created snapshot from display

--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,18 @@
 // Test away!
+import React from "React";
+import renderer from "react-test-renderer";
+import { render } from "react-testing-library";
+import "react-testing-library/cleanup-after-each";
+import "jest-dom/extend-expect";
+
+import Display from "./Display";
+
+describe("Display.js", () => {
+  describe("Display Snapshot", () => {
+    it("matches snapshot", () => {
+      const tree = renderer.create(<Display />);
+
+      expect(tree.toJSON()).toMatchSnapshot();
+    });
+  });
+});
